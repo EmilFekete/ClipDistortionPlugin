258f22ae5e4b5d88b4a925205b7f32f187044344
Fekete Emil <fekete.feketeemil>
2017-10-20 15:11:20 +0200

    - Init

---
+++ ClipDistortion.h
@@ -0,0 +1,20 @@
+#ifndef __CLIPDISTORTION__
+#define __CLIPDISTORTION__
+
+#include "IPlug_include_in_plug_hdr.h"
+
+class ClipDistortion : public IPlug
+{
+public:
+  ClipDistortion(IPlugInstanceInfo instanceInfo);
+  ~ClipDistortion();
+
+  void Reset();
+  void OnParamChange(int paramIdx);
+  void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+
+private:
+  double mGain;
+};
+
+#endif
